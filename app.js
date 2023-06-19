20f8dd2263673f5184514595f9a877cc38cdd9b2
Deepak Mittal <deepakmittal@Deepaks-MacBook-Air.local>
2023-06-19 15:48:30 +0530

    added path

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
+const path=require("path");
 
 const app = express();
 
 app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
